c59d6c6787cdb7e1e4da93331d90aa5acaa55ee4
mazkdevf <79049205+mazkdevf@users.noreply.github.com>
2024-01-09 03:11:40 +0200

    Exclude unnecessary folders and update contribution guidelines

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("HostsRemover")]
-[assembly: AssemblyDescription("Created for KeyAuth Authentication to fix Hello Error.")]
+[assembly: AssemblyDescription("Crafted for Seamless KeyAuth Authentication, Specifically Designed to Address 'Hello' Error")]
 [assembly: AssemblyProduct("HostsRemover")]
-[assembly: AssemblyCopyright("Copyright © mazkdevf 2022")]
+[assembly: AssemblyCopyright("Copyright © mazkdevf 2024")]
 
 [assembly: Guid("41bf5fe7-4374-4797-bdd2-4311f21b899b")]
 [assembly: AssemblyVersion("1.0.0.0")]
